c5e1e9663b178e6caa381a41d626f47850aaa1f2
Weliton <weliton@areacentral.com.br>
2020-12-09 02:21:45 -0300

    Crud produtos

---
+++ alterar.php
@@ -0,0 +1,34 @@
+<?php
+require_once('Conexao.php');
+$oDb = new Conexao();
+$link = $oDb->Conectar();
+session_start();
+
+$sql = '
+  UPDATE PRODUTOS
+     SET NOMEPRODUTO = "%s"
+       , IDFORNECEDOR = %s
+       , IDCATEGORIA = %s
+       , QUANTIDADEPORUNIDADE = "%s"
+       , PRECOUNITARIO = %s
+   WHERE IDPRODUTO = %s
+';
+
+$sql = sprintf(
+  $sql
+  , $_POST['nome']
+  , $_POST['fornecedor']
+  , $_POST['categoria']
+  , $_POST['quantidade']
+  , number_format($_POST['preco'], 2,'.','')
+  , $_POST['id']
+);
+var_dump($sql);
+if (mysqli_query($link, $sql)) {
+  header("Location: produtos.php");
+  exit;
+} else {
+  header(sprintf("Location: form-produto.php?produto=%s", $_POST['id']));
+  exit;
+}
+?>
---
+++ excluir.php
@@ -0,0 +1,12 @@
+<?php
+require_once('Conexao.php');
+$oDb = new Conexao();
+$link = $oDb->Conectar();
+
+$sql = sprintf("UPDATE PRODUTOS SET DESCONTINUADO = 'T' WHERE IDProduto = %s" , $_POST['IDProduto']);
+
+if (mysqli_query($link, $sql)) {
+  echo json_encode(['status' => 1]);
+} else {
+  echo json_encode(['status' => 0]);
+}
